2149e853c3c1a218a7991e730c74ce93aef53e28
DanielCruzMagalhaes <danielcruzmagalhaes@gmail.com>
2023-08-23 21:56:26 -0300

    error

--- Ex/setup/sit/urls.py
+++ Ex/setup/sit/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name= 'index'),
-    path("pagina/", views.pagina, name= "pagina")
+    path("pagina/", views.pagina, name= "pagina"),
+    path("produtos/", views.produtos, name= "produtos"),
+    path("carrinho/", views.carrinho, name= "carrinho")
 ]
